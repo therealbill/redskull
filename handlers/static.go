f1ef78570542d3680d6d69605c8810454594526f
bill5244 <bill.anderson@rackspace.com>
2014-10-06 12:42:26 -0500

    Big comment cleanup, part 1.

---
+++ handlers/static.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/zenazn/goji/web"
+)
+
+const STATIC_URL string = "http://redskull.iamtherealbill.com/static/"
+const STATIC_ROOT string = "html/static/"
+
+// Static serves static content such as images, JS and CSS files
+func Static(c web.C, w http.ResponseWriter, req *http.Request) {
+	//log.Printf("STATIC_URL: %s, %d", STATIC_URL, len(STATIC_URL))
+	//log.Printf("PATH: %s, %d", req.URL.Path, len(STATIC_URL))
+	static_file := req.URL.Path[7:]
+	if len(static_file) != 0 {
+		f, err := http.Dir(STATIC_ROOT).Open(static_file)
+		if err == nil {
+			content := io.ReadSeeker(f)
+			http.ServeContent(w, req, static_file, time.Now(), content)
+			return
+		}
+		log.Print(err)
+	}
+	http.NotFound(w, req)
+}
