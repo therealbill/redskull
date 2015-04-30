81f736343c461591c484a1c4476f54de476841aa
therealbill <therealbill@me.com>
2015-04-30 12:27:04 -0500

    skipping nil slaves in pod handler

--- handlers/pod.go
+++ handlers/pod.go
@@ -80,7 +80,11 @@ func ShowPod(c web.C, w http.ResponseWriter, r *http.Request) {
 
 	eligibleSlaves := 0
 	for _, slave := range pod.Master.Slaves {
-		slave.UpdateData()
+		up, err := slave.UpdateData()
+		if err != nil {
+			log.Print("Error on slave.UpdateData() %s", err.Error())
+			continue
+		}
 		if slave.MaxMemory <= pod.Master.MaxMemory {
 			slave.HasEnoughMemoryForMaster = true
 		} else {
